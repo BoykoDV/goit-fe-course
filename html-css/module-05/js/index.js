c78817b9df68ebea2bd6f52357084274fdd3ca97
Dima Boyko <boykod_v@ukr.net>
2019-01-22 22:23:56 +0200

    module-05-fix-1

--- html-css/module-05/js/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const modal = document.querySelector(".experiment-js");
-const btn = document.querySelectorAll("btn-fix");
-
-btn.forEach(el => {
-  el.addEventListener("click", () => modal.classList.toggle(".slaider-ctrl-next"));
-});
